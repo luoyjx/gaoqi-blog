85c13174fa8ee5542f74cf0c5bc2d779dadf175e
luoyjx <yanjixiong@shimo.im>
2019-11-05 00:02:36 +0800

    fix: models eslint

--- models/reply.js
+++ models/reply.js
@@ -4,7 +4,6 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 var ObjectId = Schema.ObjectId
-var config = require('../config')
 
 var ReplySchema = new Schema({
   content: { type: String },
--- models/tag.js
+++ models/tag.js
@@ -3,7 +3,6 @@
  */
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
-var ObjectId = Schema.ObjectId
 
 var TagSchema = new Schema({
   name: { type: String },
