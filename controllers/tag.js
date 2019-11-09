8d98658d85c75cc5fda34837949d0a95d516ff39
luoyjx <yanjixiong@shimo.im>
2019-11-09 15:12:20 +0800

    refactor: tag controller

--- controllers/tag.js
+++ controllers/tag.js
@@ -2,7 +2,7 @@
  * tag controller
  */
 
-var Promise = require('bluebird')
+var Bluebird = require('bluebird')
 var Tag = require('../dao').Tag
 var Post = require('../dao').Post
 var validator = require('validator')
@@ -15,40 +15,39 @@ var config = require('../config')
  * @param res
  * @param next
  */
-exports.index = function (req, res, next) {
-  var path = req.path
-  var page = req.query.page ? parseInt(req.query.page, 10) : 1
+exports.index = async (req, res, next) => {
+  const path = req.path
+  let page = req.query.page ? parseInt(req.query.page, 10) : 1
   page = page > 0 ? page : 1
 
-  var limit = 20
-  var options = { skip: (page - 1) * limit, limit: limit, sort: '-post_count' }
+  const limit = 20
+  const options = { skip: (page - 1) * limit, limit: limit, sort: '-post_count' }
 
-  Promise
-    .all([
+  try {
+    const [tags, pages] = await Bluebird.all([
       Tag.getAllTagsByQuery({}, options)
-        .then(function (tags) {
-          return Promise.map(tags, function (tag) {
+        .then((tags) => {
+          return Promise.map(tags, (tag) => {
             tag.description = cutter.shorter(tag.description, 200)
             return tag
           })
         }),
       Tag.getCountByQuery({})
-        .then(function (allCount) {
+        .then((allCount) => {
           return Promise.resolve(Math.ceil(allCount / limit))
         })
     ])
-    .spread(function (tags, pages) {
-      res.render('tag/list', {
-        base: path,
-        current_page: page,
-        pages: pages,
-        tags: tags,
-        title: '标签 - 第' + page + '页'
-      })
+
+    res.render('tag/list', {
+      base: path,
+      current_page: page,
+      pages: pages,
+      tags: tags,
+      title: '标签 - 第' + page + '页'
     })
-    .catch(function (err) {
-      next(err)
-    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 /**
@@ -57,14 +56,14 @@ exports.index = function (req, res, next) {
  * @param res
  * @param next
  */
-exports.getTagByName = function (req, res, next) {
-  var path = req.path
-  var page = req.query.page ? parseInt(req.query.page, 10) : 1
-  var name = validator.trim(req.params.name)
+exports.getTagByName = async (req, res, next) => {
+  const path = req.path
+  const page = req.query.page ? parseInt(req.query.page, 10) : 1
+  let name = validator.trim(req.params.name)
   name = validator.escape(name)
-  var limit = config.list_topic_count
+  const limit = config.list_topic_count
 
-  var errorInfo = ''
+  let errorInfo = ''
   if (name.length === '') {
     errorInfo = 'tag名称不能为空'
   }
@@ -74,10 +73,10 @@ exports.getTagByName = function (req, res, next) {
   }
 
   // post options
-  var options = { skip: (page - 1) * limit, limit: limit, sort: '-create_at' }
+  const options = { skip: (page - 1) * limit, limit: limit, sort: '-create_at' }
 
-  Promise
-    .all([
+  try {
+    const [tag, posts, pages] = await Bluebird.all([
       Tag.getTagByName(name),
       Post.getPostsByQuery({ tags: name }, options),
       Post.getCountByQuery({ tags: name })
@@ -85,23 +84,22 @@ exports.getTagByName = function (req, res, next) {
           return Promise.resolve(Math.ceil(allCount / limit))
         })
     ])
-    .spread(function (tag, posts, pages) {
-      if (!tag) {
-        return res.wrapRender('notify/notify', { error: '该标签可能已经去了火星' })
-      }
 
-      tag.short_desc = cutter.shorter(tag.description, 200)
+    if (!tag) {
+      return res.wrapRender('notify/notify', { error: '该标签可能已经去了火星' })
+    }
 
-      res.wrapRender('tag/index', {
-        title: name + ' 第' + page + '页',
-        tag: tag,
-        posts: posts.length === 0 ? [] : posts,
-        base: path,
-        current_page: page,
-        pages: pages
-      })
+    tag.short_desc = cutter.shorter(tag.description, 200)
+
+    res.wrapRender('tag/index', {
+      title: name + ' 第' + page + '页',
+      tag: tag,
+      posts: posts.length === 0 ? [] : posts,
+      base: path,
+      current_page: page,
+      pages: pages
     })
-    .catch(function (err) {
-      next(err)
-    })
+  } catch (error) {
+    next(error)
+  }
 }
