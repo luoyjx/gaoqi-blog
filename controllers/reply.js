9cff74f25098145c82c3183b34c9a7f6af3da82d
luoyjx <yanjixiong@shimo.im>
2019-11-09 14:33:32 +0800

    refactor: reply controller

--- controllers/reply.js
+++ controllers/reply.js
@@ -2,7 +2,7 @@
  * reply controller
  */
 
-const Promise = require('bluebird')
+const Bluebird = require('bluebird')
 const validator = require('validator')
 
 const at = require('../common/at')
@@ -12,66 +12,55 @@ const { User, Post, Reply } = require('../dao')
 /**
  * 添加回复
  */
-exports.add = function (req, res, next) {
-  var content = req.body.r_content
-  var postId = req.params._id
-  var replyId = req.body.reply_id
+exports.add = async (req, res, next) => {
+  const content = req.body.r_content
+  const postId = req.params._id
+  const replyId = req.body.reply_id
 
-  var str = validator.trim(content)
+  const str = validator.trim(content)
   if (str === '') {
     res.status(422)
     return res.render('notify/notify', { error: '回复内容不能为空！' })
   }
 
-  var _post
+  try {
+    const _post = await Post.getPost(postId)
 
-  Post
-    .getPost(postId)
-    .then(function (postFind) {
-      if (postFind.lock) {
-        return res.status(403).wrapSend('此主题已锁定。')
-      }
+    if (_post.lock) {
+      return res.status(403).wrapSend('此主题已锁定。')
+    }
 
-      _post = postFind
+    const userFind = await User.getUserById(_post.author_id)
+    const [reply] = await Bluebird.all([
+      Reply.newAndSave(content, postId, req.session.user._id, replyId)
+        .then(function (reply) {
+          // 发送at消息，并防止重复 at 作者
+          const newContent = content.replace('@' + userFind.login_name + ' ', '')
+          at.sendMessageToMentionUsers(newContent, postId, req.session.user._id, reply._id, userFind.login_name, _post.title)
+          _post.reply_count += 1
+          _post.update_at = new Date()
+          _post.last_reply_at = new Date()
+          _post.last_reply = req.session.user._id
+          _post.save()
+          return Promise.resolve(reply)
+        })
+        .then(function (reply) {
+          if (_post.author_id.toString() !== req.session.user._id.toString()) {
+            message.sendReplyMessage(_post.author_id, req.session.user._id, _post._id, reply._id)
+          }
+          return Promise.resolve(reply)
+        }),
+      User.getUserById(req.session.user._id)
+        .then(function (user) {
+          user.score += 5
+          user.reply_count += 1
+          user.save()
+          req.session.user = user
+        })
+    ])
 
-      return User.getUserById(postFind.author_id)
-    })
-    .then(function (userFind) {
-      return Promise
-        .all([
-          Reply
-            .newAndSave(content, postId, req.session.user._id, replyId)
-            .then(function (reply) {
-              // 发送at消息，并防止重复 at 作者
-              var newContent = content.replace('@' + userFind.login_name + ' ', '')
-              at.sendMessageToMentionUsers(newContent, postId, req.session.user._id, reply._id, userFind.login_name, _post.title)
-              _post.reply_count += 1
-              _post.update_at = new Date()
-              _post.last_reply_at = new Date()
-              _post.last_reply = req.session.user._id
-              _post.save()
-              return Promise.resolve(reply)
-            })
-            .then(function (reply) {
-              if (_post.author_id.toString() !== req.session.user._id.toString()) {
-                message.sendReplyMessage(_post.author_id, req.session.user._id, _post._id, reply._id)
-              }
-              return Promise.resolve(reply)
-            }),
-          User
-            .getUserById(req.session.user._id)
-            .then(function (user) {
-              user.score += 5
-              user.reply_count += 1
-              user.save()
-              req.session.user = user
-            })
-        ])
-    })
-    .spread(function (reply) {
-      res.redirect('/p/' + postId + '#' + reply._id)
-    })
-    .catch(function (err) {
-      next(err)
-    })
+    res.redirect('/p/' + postId + '#' + reply._id)
+  } catch (err) {
+    next(err)
+  }
 }
