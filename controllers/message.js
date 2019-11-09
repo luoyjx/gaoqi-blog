d6f7454fa1119e4d5bc9a36532c8f43e8c3fdc44
luoyjx <yanjixiong@shimo.im>
2019-11-09 12:50:42 +0800

    refactor: message controller

--- controllers/message.js
+++ controllers/message.js
@@ -2,32 +2,34 @@
  * 消息 controller
  */
 
-var Promise = require('bluebird')
-var Message = require('../dao').Message
+const Bluebird = require('bluebird')
+const { Message } = require('../dao')
 
-exports.index = function (req, res, next) {
-  var userId = req.session.user._id
+exports.index = async (req, res, next) => {
+  const userId = req.session.user._id
 
-  Promise
-    .all([
-      Message.getReadMessagesByUserId(userId),
-      Message.getUnreadMessageByUserId(userId)
-    ])
-    .spread(function (hasRead, hasNotRead) {
-      Message.updateMessagesToRead(userId, hasNotRead)
-      return Promise
-        .all([
-          Promise.map(hasRead, function (doc) { return Message.getMessageRelations(doc) }),
-          Promise.map(hasNotRead, function (doc) { return Message.getMessageRelations(doc) })
-        ])
-    })
-    .spread(function (hasRead, hasNotRead) {
-      hasRead = hasRead.filter(function (doc) {
-        return !doc.is_invalid
-      })
-      hasNotRead = hasNotRead.filter(function (doc) {
-        return !doc.is_invalid
-      })
-      res.render('message/index', { has_read_messages: hasRead, hasnot_read_messages: hasNotRead })
-    })
+  const [hasRead, hasNotRead] = await Bluebird.all([
+    Message.getReadMessagesByUserId(userId),
+    Message.getUnreadMessageByUserId(userId)
+  ])
+
+  // update async
+  Message.updateMessagesToRead(userId, hasNotRead)
+
+  let [hasReadMsgs, hasNotReadMsgs] = await Bluebird.all([
+    Bluebird.map(hasRead, (doc) => { return Message.getMessageRelations(doc) }),
+    Bluebird.map(hasNotRead, (doc) => { return Message.getMessageRelations(doc) })
+  ])
+
+  hasReadMsgs = hasReadMsgs.filter(function (doc) {
+    return !doc.is_invalid
+  })
+  hasNotReadMsgs = hasNotReadMsgs.filter(function (doc) {
+    return !doc.is_invalid
+  })
+
+  res.render('message/index', {
+    has_read_messages: hasReadMsgs,
+    hasnot_read_messages: hasNotReadMsgs
+  })
 }
